94d58154e29e908e667df3e26f9d2f24b8764933
variousnabil <varionabilr@gmail.com>
2020-08-25 19:36:39 +0700

    new user new exercise

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 const MONGO_URI = process.env.MONGO_URI;
@@ -11,31 +12,56 @@ var db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
     app.listen(8000);
+    app.use(bodyParser.json());
     const Schema = mongoose.Schema;
+
     const userSchema = new Schema({
         username: String,
         exercises: [{
             description: { type: String, required: true },
             duration: { type: Number, required: true },
-            date: { type: Date, default: Date.now }
+            date: { type: Number, default: Date.getTime }
         }]
     });
     const User = mongoose.model('User', userSchema);
 
     // new user
     app.post('/api/exercise/new-user', (req, res) => {
+        const username = req.body.username;
         User.create([{
-            username: 'variousnabil',
-            exercises: [{ description: 'read books', duration: 60 }]
+            username,
+            exercises: []
         }], (err, data) => {
             if (err) return res.json({ err });
             res.json({ data });
-        })
+        });
     });
 
     // new exercise
     app.post('/api/exercise/add', (req, res) => {
-
+        User.findById(req.body.userId, (err, data) => {
+            if (err) return console.log(err);
+            let newExercise = {};
+            if (req.body.date === undefined) {
+                newExercise = {
+                    description: req.body.description,
+                    duration: req.body.duration,
+                    date: new Date().getTime()
+                };
+            } else {
+                newExercise = {
+                    description: req.body.description,
+                    duration: req.body.duration,
+                    date: new Date(req.body.date).getTime()
+                };
+            }
+            data.exercises.push(newExercise);
+            const user = new User(data);
+            user.save((err, data) => {
+                if (err) return res.json({ err });
+                res.json(data);
+            });
+        });
     });
 
     // get exercises
